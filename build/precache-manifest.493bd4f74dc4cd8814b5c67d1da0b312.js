512550b21f3a9e906627bf91d252cdbdd14a72e8
Bruno <bruno.1997.borges@gmail.com>
2019-03-27 11:34:51 -0300

    Firts Commit

---
+++ build/precache-manifest.493bd4f74dc4cd8814b5c67d1da0b312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "adc978b6036cb61c9459",
+    "url": "/static/css/main.41c0ca39.chunk.css"
+  },
+  {
+    "revision": "adc978b6036cb61c9459",
+    "url": "/static/js/main.812eed9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7a07c6b5d63e01ec4fa",
+    "url": "/static/css/2.f4dc99fa.chunk.css"
+  },
+  {
+    "revision": "f7a07c6b5d63e01ec4fa",
+    "url": "/static/js/2.b007902f.chunk.js"
+  },
+  {
+    "revision": "d51cd2f83bd657eaae64c54547bc5373",
+    "url": "/static/media/logo.d51cd2f8.svg"
+  },
+  {
+    "revision": "0710917f6bb18d7305f8c349336d2052",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar'
+import RecipeItem from './RecipeItem'
+import recipes from '../sample_data/recipes.json'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.recipes = recipes.results;
+    this.state = {
+      searchString: '',
+      results: recipes.results
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount()
+  {
+    this.getData();
+  }
+
+    getData(){
+      let searchString = (this.state.searchString || "").trim();
+
+      this.setState({
+        results: recipes.results.filter((recipe) => {
+          let propertiesFilter = recipe.title.toLowerCase() + recipe.ingredients.toLowerCase()
+          return propertiesFilter.indexOf(
+            searchString.toLowerCase()
+          ) !== -1
+        })}
+      )
+  
+      this.recipes = this.state.results.map((result, i) =>
+        <RecipeItem key={i} thumbnail={result.thumbnail} 
+          title={this.getHighlightedText(result.title, this.state.searchString)} 
+          ingredients={this.getHighlightedText(result.ingredients, this.state.searchString)} />
+      );
+    }
+  
+    getHighlightedText(text, higlight) {
+      var parts = text.split(new RegExp(`(${higlight})`, 'gi'));
+      return <span>{parts.map(part => part.toLowerCase() === higlight.toLowerCase() ? <mark key={Math.random()}>{part}</mark> : part)}</span>;
+    }
+  
+    handleChange(event) {
+      this.setState({
+        searchString: event.target.value
+      }, () => {
+        this.getData()      
+      })
+    }
+  
+    render() { 
+      return (
+        <div className="App">
+          <Navbar state={this.state} handleChange={this.handleChange} />
+          <div className="container mt-10">
+            <div className="row">
+              {this.recipes}
+            </div>
+          </div>
+        </div>
+      );
+    }
+}
+
+export default App;
+
